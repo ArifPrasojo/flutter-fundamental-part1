58b333cfad1726c0070f590bb995a77ae6f42bee
username Anda <arifprasojo999@email.com>
2024-10-10 00:23:52 +0700

    Langkah 3: Scaffold Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,93 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/basic_widgets/text_widget.dart';
-import 'package:hello_world/basic_widgets/image_widget.dart';
-import 'package:flutter/cupertino.dart';
+// import 'package:hello_world/basic_widgets/text_widget.dart';
+// import 'package:hello_world/basic_widgets/image_widget.dart';
+// import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHomePage(title: 'My Increment App with Image'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            Image.asset(
+              'assets/image.png', // Path gambar yang ada di folder assets
+              width: 200, // Sesuaikan ukuran gambar
+              height: 200,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+          height: 50.0,
+          color: Colors.red[100],
+          child: const Center(
+            child: Text("This is BottomAppBar"),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
 
 //Test-Widgets
 // void main() {
@@ -167,32 +253,34 @@ import 'package:flutter/cupertino.dart';
 //     );
 //   }
 // }
-void main() {
-  runApp(const FabWidget());
-}
 
-class FabWidget extends StatelessWidget {
-  const FabWidget({Key? key}) : super(key: key);
+//fab-widget
+// void main() {
+//   runApp(const FabWidget());
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Aksi saat tombol FAB ditekan
-            // Anda dapat menambahkan kode di sini
-          },
-          child: const Icon(Icons.thumb_up),
-          backgroundColor: Colors.pink,
-        ),
-        appBar: AppBar(
-          title: const Text('Contoh FloatingActionButton'),
-        ),
-        body: const Center(
-          child: Text('Tekan tombol di bawah'),
-        ),
-      ),
-    );
-  }
-}
+// class FabWidget extends StatelessWidget {
+//   const FabWidget({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () {
+//             // Aksi saat tombol FAB ditekan
+//             // Anda dapat menambahkan kode di sini
+//           },
+//           child: const Icon(Icons.thumb_up),
+//           backgroundColor: Colors.pink,
+//         ),
+//         appBar: AppBar(
+//           title: const Text('Contoh FloatingActionButton'),
+//         ),
+//         body: const Center(
+//           child: Text('Tekan tombol di bawah'),
+//         ),
+//       ),
+//     );
+//   }
+// }
